1694f4d21a2b22baafe2cc19561275bdd8b62fa8
taskie <t@skie.jp>
2017-02-07 10:38:00 +0900

    nkfy: convert gzip file

--- contrib/nkfy.sh
+++ contrib/nkfy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# requires: nkf, gzip
 set -eu
 
 if (( $# < 1 )); then
@@ -11,10 +12,16 @@ for oldfile in $(find "$1" -name '*.euc_jp')
 do
     newfile="${oldfile%%.euc_jp}"
     bakfile="$newfile.bak"
+    # create backup file
     if [[ -f "$newfile" ]] && [[ ! -f "$bakfile" ]]; then
         echo "mv \"$newfile\" \"$bakfile\""
         mv "$newfile" "$bakfile"
     fi
-    echo "nkf -E -w80 \"$oldfile\" > \"$newfile\""
-    nkf -E -w80 "$oldfile" > "$newfile"
+    if [[ -z "${newfile%%*.gz}" ]]; then
+        echo "gz -cd \"$oldfile\" | nkf -E -w80 | gz -c > \"$newfile\""
+        gzip -cd "$oldfile" | nkf -E -w80 | gzip -c > "$newfile"
+    else
+        echo "nkf -E -w80 \"$oldfile\" > \"$newfile\""
+        nkf -E -w80 "$oldfile" > "$newfile"
+    fi
 done
